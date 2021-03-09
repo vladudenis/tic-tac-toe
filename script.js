ef40803156a8cc9eca2fd4cd6863eb3c96c20adb
Denis V <vladudenis09@gmail.com>
2021-03-09 21:23:01 +0100

    Finally fixed everything.

--- script.js
+++ script.js
@@ -3,19 +3,17 @@ const Game = (function(){
     let _turn = "X";
     let _win = false;
 
-    function _hasWon(){ //something isn't working here
+    function _hasWon(){
         if(_gameboard[0] == [_turn, _turn, _turn]
             || _gameboard[1] == [_turn, _turn, _turn]
             || _gameboard[2] == [_turn, _turn, _turn]
             || (_gameboard[0][0] == _turn && _gameboard[1][1] == _turn && _gameboard[2][2] == _turn)
             || (_gameboard[0][2] == _turn && _gameboard[1][1] == _turn && _gameboard[2][0] == _turn)
-            || (_gameboard[0][0] == _turn && _gameboard[0][1] == _turn && _gameboard[0][2] == _turn)
-            || (_gameboard[1][0] == _turn && _gameboard[1][1] == _turn && _gameboard[1][2] == _turn)
-            || (_gameboard[2][0] == _turn && _gameboard[2][1] == _turn && _gameboard[2][2] == _turn)){
-                console.log("yes");
+            || (_gameboard[0][0] == _turn && _gameboard[1][0] == _turn && _gameboard[2][0] == _turn)
+            || (_gameboard[0][1] == _turn && _gameboard[1][1] == _turn && _gameboard[2][1] == _turn)
+            || (_gameboard[0][2] == _turn && _gameboard[1][2] == _turn && _gameboard[2][2] == _turn)){
                 return true;
         }else{
-                console.log("no");
                 return false;
         }
     }
@@ -23,39 +21,24 @@ const Game = (function(){
     function makeMove(e){
         e.target.classList.remove("show-move");
         e.target.classList.add("made-move");
-        switch(e.target.className){
-            case "grid-item 00":
-                _gameboard[0][0] = _turn;
-                break;
-            case "grid-item 01":
-                _gameboard[0][1] = _turn;
-                break;
-            case "grid-item 02":
-                _gameboard[0][2] = _turn;
-                break;
-            case "grid-item 10":
-                _gameboard[1][0] = _turn;
-                break;
-            case "grid-item 11":
-                _gameboard[1][1] = _turn;
-                break;
-            case "grid-item 12":
-                _gameboard[1][2] = _turn;
-                break;
-            case "grid-item 20":
-                _gameboard[2][0] = _turn;
-                break;
-            case "grid-item 21":
-                _gameboard[2][1] = _turn;
-                break;
-            case "grid-item 22":
-                _gameboard[2][2] = _turn;
-                break;
-        }
-        if(_turn == "X"){
-            _turn = "O";
-        }else{
-            _turn = "X";
+        if(e.target.classList.contains("00")){
+            _gameboard[0][0] = _turn;
+        }else if(e.target.classList.contains("01")){
+            _gameboard[0][1] = _turn;
+        }else if(e.target.classList.contains("02")){
+            _gameboard[0][2] = _turn;
+        }else if(e.target.classList.contains("10")){
+            _gameboard[1][0] = _turn;
+        }else if(e.target.classList.contains("11")){
+            _gameboard[1][1] = _turn;
+        }else if(e.target.classList.contains("12")){
+            _gameboard[1][2] = _turn;
+        }else if(e.target.classList.contains("20")){
+            _gameboard[2][0] = _turn;
+        }else if(e.target.classList.contains("21")){
+            _gameboard[2][1] = _turn;
+        }else if(e.target.classList.contains("22")){
+            _gameboard[2][2] = _turn;
         }
         if(_hasWon()){
             _win = true;
@@ -63,6 +46,11 @@ const Game = (function(){
             squares.forEach(square => square.removeEventListener("mouseout", Game.fadeOutMove));
             squares.forEach(square => square.removeEventListener("click", Game.makeMove));
             header.textContent = "We Have A Winner!";
+        }
+        if(_turn == "X"){
+            _turn = "O";
+        }else{
+            _turn = "X";
         }
     }
 
@@ -105,13 +93,13 @@ const Game = (function(){
     };
 })();
 
+const header = document.querySelector("header");
 const squares = document.querySelectorAll(".grid-item");
 squares.forEach(square => square.addEventListener("mouseover", Game.showMove));
 squares.forEach(square => square.addEventListener("mouseout", Game.fadeOutMove));
 squares.forEach(square => square.addEventListener("click", Game.makeMove));
 const restartBtn = document.querySelector(".restart-btn");
 restartBtn.addEventListener("click", Game.restartGame);
-const header = document.querySelector("header");
 
 /* to be added later
 const Player = () => {
